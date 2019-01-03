f9e6cbf72a5051f028a0bb377501542a6f542479
Max F. Albrecht <1@178.is>
2019-01-03 16:58:12 +0100

    Make browser visit timeout and waitUntil configurable

--- ScreenshotHelper.js
+++ ScreenshotHelper.js
@@ -3,6 +3,8 @@ const puppeteer = require('puppeteer-core');
 module.exports = ScreenshotHelper;
 
 const defaultConfig = {
+  waitUntil: 'networkidle0',
+  timeout: 10000,
 }
 
 function ScreenshotHelper(log, url, chromiumPath = "/usr/bin/chromium-browser") {
@@ -14,8 +16,10 @@ function ScreenshotHelper(log, url, chromiumPath = "/usr/bin/chromium-browser") 
 
 ScreenshotHelper.prototype.getScreenshot = async function (config) {
     config = Object.assign({}, defaultConfig, config);
-    
+
     const viewport = { width: config.width, height: config.height };
+    const gotoOpts = { waitUntil: config.waitUntil, timeout: config.timeout };
+    const screenshotOpts = { type: "jpeg" };
 
     if (!this.browser) {
         this.log("Starting new instance of Chromium: " + this.chromiumPath);
@@ -33,8 +37,8 @@ ScreenshotHelper.prototype.getScreenshot = async function (config) {
     this.log("Setting Viewport to " + viewport.width + "x" + viewport.height);
     await page.setViewport(viewport);
     this.log("Going to page: " + this.url);
-    await page.goto(this.url, {waitUntil: 'networkidle0', timeout: 10000});
-    const screenshot = await page.screenshot({type: "jpeg"});
+    await page.goto(this.url, gotoOpts);
+    const screenshot = await page.screenshot(screenshotOpts);
     this.log("Created screenshot");
     page.close();
     return screenshot;
